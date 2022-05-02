adbdd195c3e177142b1748e7c0e2a0f8bcf7d479
PRO <54608551+PRO-2684@users.noreply.github.com>
2022-05-02 19:45:29 +0800

    Rename; Simple argument parse.

--- bili-crawer.py
+++ bili-crawer.py
@@ -1,6 +1,7 @@
+import argparse
 import dm_pb2 as Danmaku
 from requests import Session
-from json import dump
+from re import search
 
 
 class Video:
@@ -57,14 +58,34 @@ class Video:
         r = self.session.get(api, params=param)
         return r.json()
 
+
+class ArgumentException(Exception):
+    """Something wrong with provided parameters."""
+    def __init__(self, *args):
+        self.args = args
+
 if __name__ == "__main__":
-    danmakus = Video("BV1si4y1k7eG").fetch_danmakus()
-    with open("danmaku.txt", "w", encoding="utf-8") as f:
-        for danmaku in danmakus:
-            f.write(danmaku.content + "\n")
-    comments = Video("BV1si4y1k7eG").fetch_comments()
-    with open('comments.json', 'w', encoding="utf-8") as f:
-        dump(comments, f, ensure_ascii=False)
-
-
+    parser = argparse.ArgumentParser(description='A simple script for Bilibili crawling.')
+    parser.add_argument('target', help='A string with target BV id in it, for example "BV1GJ411x7h7".')
+    parser.add_argument('-v', '--video', help='If included, download video(s).', action='store_true')
+    parser.add_argument('-c', '--comment', help='If included, download comments.', action='store_true')
+    parser.add_argument('-d', '--danmaku', help='If included, download danmakus.', action='store_true')
+    parser.add_argument('--debug', help='Use debug mode. i.e. show more info.', action='store_true')
+    args = parser.parse_args()
+    bv = ''
+    m = search(r"([Bb][Vv][A-Za-z0-9]+)", args.target)
+    if m:
+        bv = m.groups()[0]
+    else:
+        raise ArgumentException('No valid BV found.')
+    bv = bv[:2].upper() + bv[2:]
+    print(bv)
+    # Example usage:
+    # danmakus = Video("BV1si4y1k7eG").fetch_danmakus()
+    # with open("danmaku.txt", "w", encoding="utf-8") as f:
+    #     for danmaku in danmakus:
+    #         f.write(danmaku.content + "\n")
+    # comments = Video("BV1si4y1k7eG").fetch_comments()
+    # with open('comments.json', 'w', encoding="utf-8") as f:
+    #     dump(comments, f, ensure_ascii=False)
 
