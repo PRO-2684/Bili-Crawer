887801b4f1468f02b0f96e8fa521e2a8ba3c8153
PRO <54608551+PRO-2684@users.noreply.github.com>
2022-05-11 17:23:58 +0800

    Crawl basic info of a video

--- bili-crawer.py
+++ bili-crawer.py
@@ -1,4 +1,4 @@
-import argparse
+from argparse import ArgumentParser
 import dm_pb2 as Danmaku
 from requests import Session
 from re import search
@@ -13,21 +13,38 @@ class Video:
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.55"
             }
         )
-        self.cid = self.__get_cid()
-
-    def __get_cid(self) -> int:
-        r = self.session.get(
-            f"https://api.bilibili.com/x/player/pagelist?bvid={self.bv}&jsonp=jsonp"
-        )
-        data = r.json()
-        assert data["code"] == 0, "Get cid failed! Response: " + r.text
-        return data["data"][0]["cid"]
+        info = self.fetch_info()
+        self.title = info["title"]
+        self.uploader = info["owner"]["name"]
+        self.statistics = {
+            attr: info["stat"][attr] for attr in {"view", "favorite", "coin", "like"}
+        }  # 'danmaku', 'share', 'reply',
+        self.parts = [
+            {
+                "id": part["page"],
+                "cid": part["cid"],
+                "title": part["part"],
+                "duration": part["duration"],
+                "resolution": {
+                    "width": part["dimension"]["width"],
+                    "height": part["dimension"]["height"],
+                },
+            }
+            for part in info["pages"]
+        ]
 
     def fetch_info(self):
-        api = 'http://api.bilibili.com/x/web-interface/view'
-        return
+        """Returns basic info of given video."""
+        params = {"bvid": self.bv}
+        r = self.session.get(
+            "http://api.bilibili.com/x/web-interface/view", params=params
+        ).json()
+        assert r["code"] == 0, "Error fetching video info: " + r["message"]
+        return r["data"]
 
-    def fetch_danmakus(self, serial: int = 1, avid: int = 0, type_: int = 1) -> list[Danmaku.DanmakuElem]:
+    def fetch_danmakus(
+        self, serial: int = 1, avid: int = 0, type_: int = 1
+    ) -> list[Danmaku.DanmakuElem]:
         """Returns a list of `DanmakuElem`. Remember to use `as_utf8`."""
         api = "https://api.bilibili.com/x/v2/dm/web/seg.so"
         param = {"type": type_, "oid": self.cid, "segment_index": serial}
@@ -41,51 +58,69 @@ class Video:
 
     def fetch_comments(self, page: int = 0, mode: int = 3):
         """Returns a list of comments in `dict` format.
-    
+
         | Mode | Meaning          |
         | ---- | ---------------- |
         | 0/3  | Order by hotness |
         | 1    | Hotness & time   |
         | 2    | Time             |
         """
-        api = 'http://api.bilibili.com/x/v2/reply/main'
-        param = {
-            'type': 1,
-            'oid': 552506117, # self.oid???
-            'mode': mode,
-            'next': page
-        }
+        api = "http://api.bilibili.com/x/v2/reply/main"
+        param = {"type": 1, "oid": 552506117, "mode": mode, "next": page}  # self.oid???
         r = self.session.get(api, params=param)
         return r.json()
 
 
-class ArgumentException(Exception):
-    """Something wrong with provided parameters."""
-    def __init__(self, *args):
-        self.args = args
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='A simple script for Bilibili crawling.')
-    parser.add_argument('target', help='A string with target BV id in it, for example "BV1GJ411x7h7".')
-    parser.add_argument('-v', '--video', help='If included, download video(s).', action='store_true')
-    parser.add_argument('-c', '--comment', help='If included, download comments.', action='store_true')
-    parser.add_argument('-d', '--danmaku', help='If included, download danmakus.', action='store_true')
-    parser.add_argument('--debug', help='Use debug mode. i.e. show more info.', action='store_true')
+    # Parse commandline arguments.
+    parser = ArgumentParser(description="A simple script for Bilibili crawling.")
+    parser.add_argument(
+        "target",
+        help='A string with target BV id in it, for example "prefixbV1GJ411x7h7suffix".',
+    )
+    parser.add_argument(
+        "-o", "--output", help="Output directory.", default="./", required=False
+    )
+    parser.add_argument(
+        "-v", "--video", help="If included, download video(s).", action="store_true"
+    )
+    parser.add_argument(
+        "-c", "--comment", help="If included, download comments.", action="store_true"
+    )
+    parser.add_argument(
+        "-d", "--danmaku", help="If included, download danmakus.", action="store_true"
+    )
+    parser.add_argument(
+        "--debug", help="Use debug mode. i.e. show more info.", action="store_true"
+    )
     args = parser.parse_args()
-    bv = ''
-    m = search(r"([Bb][Vv][A-Za-z0-9]+)", args.target)
+    bv = ""
+    # Normalize to "BVxxxxxxxxxx".
+    m = search(r"([Bb][Vv][A-Za-z0-9]{10})", args.target)
     if m:
         bv = m.groups()[0]
     else:
-        raise ArgumentException('No valid BV found.')
+        parser.error("No valid BV found.")
     bv = bv[:2].upper() + bv[2:]
-    print(bv)
+    print("Target video:", bv)
+    video = Video(bv)
+    print("Title:", video.title)
+    print("Up:", video.uploader)
+    for k, v in video.statistics.items():
+        print(f"{k.capitalize()}: {v}")
+    print("Video parts:")
+    for part in video.parts:
+        print("  #", part["id"])
+        print("  Title:", part["title"])
+        print("  Duration:", part["duration"])
+        print(
+            f'  Resolution: {part["resolution"]["width"]}x{part["resolution"]["height"]}'
+        )
     # Example usage:
-    # danmakus = Video("BV1si4y1k7eG").fetch_danmakus()
+    # danmakus = video.fetch_danmakus()
     # with open("danmaku.txt", "w", encoding="utf-8") as f:
     #     for danmaku in danmakus:
     #         f.write(danmaku.content + "\n")
-    # comments = Video("BV1si4y1k7eG").fetch_comments()
-    # with open('comments.json', 'w', encoding="utf-8") as f:
-    #     dump(comments, f, ensure_ascii=False)
-
+    # comments = video.fetch_comments()
+    # with open("comments.json", "w", encoding="utf-8") as f:
+    #     json.dump(comments, f, ensure_ascii=False)
